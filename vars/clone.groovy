b8ff8409f77f3bc88b7b9505a94368e64dc8cd1d
vipin kumar <96677260+vipinraperia6537@users.noreply.github.com>
2025-09-29 00:15:02 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
 def call(string url, string branch){
   echo "cloning stage"
-  git url: "https://github.com/vipinraperia6537/django-notes-app.git" , branch: "main"
+  git url: "${url}", branch: "${branch}"
   echo " code is cloned"
 }
